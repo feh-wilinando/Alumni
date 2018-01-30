c6c54b272ef512eb55d11707401f54373c67ece5
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-01-30 16:08:32 -0200

    Added Collection view

---
+++ Alumni/UIComponents/CourseCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CourseCollectionViewCell.swift
+//  Alumni
+//
+//  Created by Nando on 30/01/18.
+//  Copyright © 2018 Nando. All rights reserved.
+//
+
+import UIKit
+
+class CourseCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var codeLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    
+}
